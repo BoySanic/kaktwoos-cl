14770d3f8ae9a06e01883cd674580723802c4d35
BoySanic <boysonicrevived@gmail.com>
2020-12-12 04:54:23 -0500

    fix(kaktwoos): corrected pointer pointer to pointer

--- main.c
+++ main.c
@@ -148,7 +148,7 @@ boinc_set_min_checkpoint_period(30);
             return 1;
         }
     #else
-    retval = clGetDeviceIDs(&platform_id, CL_DEVICE_TYPE_GPU, 1, &device_ids, 1);
+    retval = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_ids, 1);
     #endif
     
     cl_context_properties cps[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id, 0};
